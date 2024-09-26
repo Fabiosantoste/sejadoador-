b17641fc2c035aba3d9b94cf698bebebfd93c3ae
Fábio <fabiosantoste@gmail.com>
2024-09-26 17:11:36 -0300

    backend

---
+++ sejadoador/src/test/java/com/hemocentro/sejadoador/SejadoadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemocentro.sejadoador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejadoadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
